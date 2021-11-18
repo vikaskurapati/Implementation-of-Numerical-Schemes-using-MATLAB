5518f8bf22643fd66c65a72efb8f304c02f48a5f
Vikas Kurapati <vikas.kurapati@tum.de>
2021-11-19 00:07:20 +0100

    Added Exercise 2, need sanity check for the solution

---
+++ exercise2/vanderpol.m
@@ -0,0 +1,17 @@
+function vanderpol()
+    function grad = grad(t, y)
+        grad = zeros(1,2);
+        grad(1) = y(2);
+        grad(2) = -y(1) + y(2)*(1-y(1)*y(1));
+    end
+
+    y_0 = [1,1];
+    dt = 0.1;
+    t_end = 20;
+    t = 0:dt:t_end;
+    y = expl_heun_vector(y_0, dt, t_end, @grad);
+    hold all
+    plot(t, y(:,1), '*', 'Color','r');
+    plot(t, y(:,2), '-', 'Color', 'b');
+    plot(y(:,1),y(:,2), 'o', 'Color','G');
+end
