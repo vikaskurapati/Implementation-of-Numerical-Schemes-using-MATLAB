05dd49ce0014ebfe3e5b2cc9e7199d088f26d3a5
Surya Vamshi Penumarthi <surya.penumarthi@tum.de>
2021-11-21 19:31:10 +0100

    optional plot added

--- exercise2/vanderpol.m
+++ exercise2/vanderpol.m
@@ -11,11 +11,26 @@ function vanderpol()
     t = 0:dt:t_end;
     y = expl_heun_vector(y_0, dt, t_end, @grad);
     figure(fig_count)
-    hold all
+    hold on
     plot(t, y(:,1), '*', 'Color','r', 'DisplayName','y vs t');
     plot(t, y(:,2), '-', 'Color', 'b', 'DisplayName','x vs t');
     plot(y(:,1),y(:,2), 'o', 'Color','G', 'DisplayName', 'x vs y');
     title('Van-der-Pol-Oscillator(Method of Heun)');
     legend('show');
+    hold off
+    fig_count = fig_count + 1;
+        
+    dxdt = @(x, y) y;
+    dydt = @(x, y) (1-x*x)*y - x;
+
+    x = -1:0.1:1;
+    y = -1:0.1:1;
+    [X, Y] = meshgrid(x,y);
+    dX = dxdt(X,Y);
+    dY = dydt(X,Y);
+    figure(fig_count)
+    quiver(X, Y, dX,dY);
+    axis tight
+
     fig_count = fig_count + 1;
 end
