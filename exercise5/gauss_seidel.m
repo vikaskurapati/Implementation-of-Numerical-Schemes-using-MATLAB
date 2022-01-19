bdd9d0ec684437907575eec26fa0c36a247f0c79
Vikas Kurapati <vikas.kurapati@tum.de>
2022-01-19 11:13:47 +0100

    Modified Gauss_seidel

--- exercise5/gauss_seidel.m
+++ exercise5/gauss_seidel.m
@@ -1,12 +1,12 @@
 function x = gauss_seidel(A, b)
-    x = zeros(size(b));
+    x = b;
     error = 1;
     for iter=1:100
-        for j = 1:size(A,1)
-            x(j) = (b(j) - sum(A(j,:)'.*x) + A(j,j)*x(j))/A(j,j);
+        for i = 1:size(A,1)
+            x(i) = (b(i) - dot(A(i,:),x) + A(i,i)*x(i))/A(i,i);
         end
         error = norm(A*x - b, 2);
-        if error > 1e-6
+        if error < 1e-6
             break;
         end
     end
